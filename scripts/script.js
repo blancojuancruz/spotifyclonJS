2465e260caff00c2ba1be491eb76a513ad784c64
blancojuancruz <99100776+blancojuancruz@users.noreply.github.com>
2022-04-14 17:25:56 -0300

    front/playPause func button

--- scripts/script.js
+++ scripts/script.js
@@ -1,97 +1,36 @@
-console.log('Welcome to my 1st JS proyect')
+// FUNCIONALIDADES
 
-function containsObject(obj, list) {
-    let i;
-    for (i = 0; i < list.length; i++) {
-        if (list[i] === obj) {
-            return true;
-        }
+let audioSong = new Audio('audio/1.mp3')
+let progressBar = document.getElementById('progressBar')
+let playPause = document.getElementById('playPause')
+
+let songs = [
+    {songName:'Desfilar mis penas', filePath:'audio/1.mp3', coverPath: 'play.svg'},
+    {songName:'Desfilar mis penas', filePath:'audio/1.mp3', coverPath: 'play.svg'},
+    {songName:'Desfilar mis penas', filePath:'audio/1.mp3', coverPath: 'play.svg'},
+    {songName:'Desfilar mis penas', filePath:'audio/1.mp3', coverPath: 'play.svg'},
+    {songName:'Desfilar mis penas', filePath:'audio/1.mp3', coverPath: 'play.svg'},
+    {songName:'Desfilar mis penas', filePath:'audio/1.mp3', coverPath: 'play.svg'},
+]
+
+playPause.addEventListener('click', () => {
+    if(audioSong.paused || audioSong.currentTime <= 0 ){
+        audioSong.play();
+        playPause.classList.remove('bi-play-circle-fill')
+        playPause.classList.add('bi-pause-circle-fill')
+    }else {
+        audioElement.pause()
+        playPause.classList.remove('bi-pause-circle-fill')
+        playPause.classList.add('bi-play-circle-fill')
     }
+})
 
-    return false;
-}
+audioSong.addEventListener('timeupdate', ()=>{
+    console.log('timeupdate');
 
-let nSongs = parseInt(prompt('Ingrese la cantidad de canciones que quiere escuchar'))
+    // CONSOLE TIMER
+    progress = parseInt((audioSong.currentTime/audioSong.duration)*100)
 
-if(!nSongs) {
-    alert('Porfavor ingrese un numero')
-    
-    nSongs = parseInt(prompt('Ingrese la cantidad de canciones que quiere escuchar'))
-} else if(nSongs < 1) {
-    alert('Porfavor ingrese un numero valido')
-    
-    nSongs = parseInt(prompt('Ingrese la cantidad de canciones que quiere escuchar'))
-} else if(nSongs > 10){
-    alert('Ingrese un numero menor a 10')
-
-    nSongs = parseInt(prompt('Ingrese la cantidad de canciones que quiere escuchar'))
-}
-
-class Cancion {
-    constructor(name, author, musicalGenre, price){
-        this.name = name
-        this.author = author
-        this.musicalGenre = musicalGenre
-        this.price = price
-    }
-}
-
-const single1 = new Cancion('Hayya Hayya (Better Together FIFA WC OFFICIAL SOUNDTRACK)', 'Trinidad Cardona, Davido y Aysha', 'Pop', 50)
-const single2 = new Cancion('Bizarrap - Nicky Jam', 'Nicky Jam', 'Reggaeton', 70)
-const single3 = new Cancion('Desfilar mis penas (prod. ASAN', 'YSY A', 'Trap', 100)
-const single4 = new Cancion('Piel de cordero', 'QUEVEDO, La pantera', 'Rap', 150)
-const single5 = new Cancion('Volando bajito', 'Duki', 'Trap', 200)
-const single6 = new Cancion('La Zona', 'Bad Bunny', 'Reggaeton', 250)
-const single7 = new Cancion('En la ciudad de la furia', 'Soda Stereo', 'Rock Nacional', 300)
-const single8 = new Cancion('Is this love', 'Bob Marley', 'Reggae', 350)
-const single9 = new Cancion('Cayo la noche', 'QUEVEDO', 'Reggaeton', 350)
-const single10 = new Cancion('FULL ICE', 'YSY A', 'Trap', 400)
-
-const listOfSingle = [single1, single2, single3, single4, single5, single6, single7, single8, single9, single10]
-
-let randomIndex
-let listElement
-let noRepeat = []
-
-for(let a = 0; a < nSongs; a++){
-    randomIndex = Math.floor(Math.random()*10)
-    listElement = listOfSingle[randomIndex]
-
-    while(containsObject(listElement, noRepeat)){
-        randomIndex = Math.floor(Math.random()*10)
-        listElement = listOfSingle[randomIndex]
-    }
-
-    noRepeat.push(listElement)
-}
-
-noRepeat.forEach(element => console.log(element.name))
-
-
-function total(a) {
-    let totalPrice = 0
-    a.map(element => {
-        totalPrice = totalPrice + element.price
-    })
-    
-    return totalPrice
-}
-
-console.log( total(noRepeat) )
-
-
-//
- 
-
-const createList = []
-let maxSongs = 10
-
-
-do {
-    const musicName = prompt('Ingrese nombre de la cancion que quiere añadir')
-    createList.push(musicName)
-    console.log(createList.length + musicName)
-} while(createList.length != maxSongs)
-
-const newList = createList.concat(['My new list'])
-alert(newList.join('\n'))
+    console.log(progress)
+    progressBar.value = progress
+})
