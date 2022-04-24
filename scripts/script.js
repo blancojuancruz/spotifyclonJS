0ea413d451181602fe119ef355f601f77d6175a0
blancojuancruz <99100776+blancojuancruz@users.noreply.github.com>
2022-04-24 00:08:38 -0300

    some func added

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   allMyMusic();
-  mainBanner();
 });
 // CARGAR AL INICIO
 
 // VARIABLES INICIALIZADAS
 let songIndex = 0;
 let audioSong = new Audio();
+const loadMusic = () => {
+  audioSong.src = `audio/${songIndex + 1}.mp3`;
+  songName.innerText = `Now Playing: ${songs[songIndex].name}`;
+  timeDuration.innerText = songs[songIndex].duration;
+  audioSong.play();
+  audioSong.currentTime = 0;
+  playPause.classList.remove("bi-play-circle-fill");
+  playPause.classList.add("bi-pause-circle-fill");
+};
 const progressBar = document.getElementById("progressBar");
 const playPause = document.getElementById("playPause");
 const songName = document.getElementById("songName");
@@ -19,6 +27,10 @@ const timeStart = document.getElementById("timerStart");
 const volumen = document.getElementsByClassName("barra_progresoVolume");
 const replay = document.getElementById("replay");
 const loopSong = document.getElementById("loopSong");
+const likedSongsModal = document.getElementById("likedSongsList");
+const likedMusicElement = document.getElementById("likedSongs");
+const likedMusic = [];
+
 // VARIABLES DECLARADAS
 
 // ARRAY DE OBJETOS (CANCIONES)
@@ -27,84 +39,84 @@ const songs = [
     name: "YSY A - DESFILAR MIS PENAS (PROD. ASAN)",
     filePath: "audio/1.mp3",
     img: "https://i.scdn.co/image/ab67616d0000b2738f2162551691a5954d928290",
-    id: "0",
+    id: 0,
     duration: "02:31",
   },
   {
-    name: "Kidd Keo - Level Up ft. Enry K, Yay (Official Video)",
+    name: "Kidd Keo - Level Up ft. Enry K, Yay",
     filePath: "audio/2.mp3",
     img: "https://i.scdn.co/image/ab67616d0000b273c5714b3429d64d2679742348",
-    id: "1",
+    id: 1,
     duration: "03:38",
   },
   {
     name: "Rauw Alejandro & Chencho Corleone - Desesperados",
     filePath: "audio/3.mp3",
     img: "https://i.scdn.co/image/ab67616d0000b2733752b28d1b65cc1ce6da67a7",
-    id: "2",
+    id: 2,
     duration: "03:51",
   },
   {
     name: "Feid, Alejo & Robi - Pantysito",
     filePath: "audio/4.mp3",
     img: "https://i.scdn.co/image/ab67616d0000b27303a184c19d99418042ddf68a",
-    id: "3",
+    id: 3,
     duration: "03:56",
   },
   {
     name: "DUKI - TOP 5 - Temporada de Reggaeton",
     filePath: "audio/5.mp3",
     img: "https://i.scdn.co/image/ab67616d0000b273142334e67b095d6aa56655b9",
-    id: "4",
+    id: 4,
     duration: "03:04",
   },
   {
     name: "DUKI - Sudor y trabajo",
     filePath: "audio/6.mp3",
     img: "https://i.scdn.co/image/ab67616d0000b273305240b65c28020de3aac8eb",
-    id: "5",
+    id: 5,
     duration: "03:16",
   },
   {
     name: "Midnight City - M83",
     filePath: "audio/7.mp3",
     img: "https://i.scdn.co/image/ab67616d0000b273b3591763154a27326b3f431a",
-    id: "6",
+    id: 6,
     duration: "04:03",
   },
   {
     name: "Save Me - LISTENBEE, Naz Tokio",
     filePath: "audio/8.mp3",
     img: "https://i.scdn.co/image/ab67616d0000b27325b3149f3280cbd97ea8dbb1",
-    id: "7",
+    id: 7,
     duration: "03:29",
   },
   {
     name: "Tuesday (feat. Danelle Sandoval)",
     filePath: "audio/9.mp3",
     img: "https://i.scdn.co/image/ab67616d0000b27312b939225600b564150c0a68",
-    id: "8",
+    id: 8,
     duration: "02:35",
   },
   {
     name: "All I Need - Ehrling",
     filePath: "audio/10.mp3",
     img: "https://i.scdn.co/image/ab67616d0000b273e903f6908b068d0f0a74a92f",
-    id: "9",
+    id: 9,
     duration: "03:10",
   },
   {
     name: "Para Siempre Diego - Los Ratones Paranoicos",
     filePath: "audio/11.mp3",
     img: "https://i.scdn.co/image/ab67616d0000b273b0289fb853c995d7bdad4e9d",
-    id: "10",
-    duration: "03:48",
+    id: 10,
+    duration: "04:38",
   },
   {
-    name: "Enemy (with JID) - from the series Arcane League of Legends",
+    name: "Imagine Dragons - Enemy (with JID)",
     filePath: "audio/12.mp3",
     img: "https://i.scdn.co/image/ab67616d0000b273d9b35d1c4d15c9de88b754a7",
-    id: "11",
+    id: 11,
     duration: "02:53",
   },
 ];
@@ -130,12 +142,34 @@ function allMyMusic() {
     cardTitle.classList.add("songName");
     cardTitle.textContent = song.name;
 
+    const divSeparate = document.createElement("div");
+    divSeparate.classList.add("separate");
+
     const cardSpan = document.createElement("span");
+    cardSpan.classList.add("playSpan");
 
     const cardIcon = document.createElement("i");
     cardIcon.id = song.id;
     cardIcon.classList.add("bi-play-circle");
     cardIcon.classList.add("playSong");
+
+    const likeSpan = document.createElement("span");
+
+    const likeSong = document.createElement("i");
+    likeSong.classList.add("likeIcon");
+    likeSong.classList.add("material-icons");
+    likeSong.innerText = "favorite";
+    likeSong.onclick = () => {
+      if (likeSong.innerText == "favorite") {
+        likeSong.innerHTML = "thumb_down";
+        addFavorites(song.id);
+        closeSideModal();
+      } else {
+        likeSong.innerHTML = "favorite";
+        removeFavorites(song.id);
+        closeSideModal();
+      }
+    };
 
     divCards.appendChild(divImg);
     divCards.appendChild(imgCard);
@@ -143,14 +177,22 @@ function allMyMusic() {
     divCards.appendChild(cardTitle);
     divCards.appendChild(cardSpan);
     divCards.appendChild(cardIcon);
+    divCards.appendChild(likeSong);
+    divCards.appendChild(divSeparate);
+    divCards.appendChild(likeSpan);
 
     divImg.appendChild(imgCard);
 
     divcardTxt.appendChild(cardTitle);
-    divcardTxt.appendChild(cardSpan);
-    divcardTxt.appendChild(cardIcon);
+    divcardTxt.appendChild(divSeparate);
+
+    divSeparate.appendChild(cardSpan);
+    divSeparate.appendChild(cardIcon);
+    divSeparate.appendChild(likeSong);
+    divSeparate.appendChild(likeSpan);
 
     cardSpan.appendChild(cardIcon);
+    likeSpan.appendChild(likeSong);
 
     cardContainer.appendChild(divCards);
   });
@@ -172,17 +214,105 @@ function allMyMusic() {
       songIndex = parseInt(e.target.id);
       e.target.classList.remove("bi-play-circle");
       e.target.classList.add("bi-pause-circle");
-      audioSong.src = `audio/${songIndex + 1}.mp3`;
-      songName.innerText = `Now Playing: ${songs[songIndex].name}`;
-      timeDuration.innerText = songs[songIndex].duration;
-      audioSong.play();
-      audioSong.currentTime = 0;
-      playPause.classList.remove("bi-play-circle-fill");
-      playPause.classList.add("bi-pause-circle-fill");
+      loadMusic();
     });
   });
 }
 // SCRIPTING CANCIONES INICIO
+const closeSideModal = () => {
+  const closeX = document.getElementById("closeList");
+  closeX.addEventListener("click", () => {
+    likedMusicElement.classList.remove("showFavoriteSongs");
+  });
+};
+
+const addFavorites = (id) => {
+  const likedSong = songs.find((song) => song.id === id);
+  likedMusic.push(likedSong);
+  showLikedMusic(likedMusic);
+};
+
+const removeFavorites = (id) => {
+  const item = likedMusic.find((song) => song.id === id);
+  const index = likedMusic.indexOf(item);
+  likedMusic.splice(index, 1);
+  showLikedMusic(likedMusic);
+};
+
+const closeList = document.createElement("span");
+closeList.classList.add("material-icons", "closeList");
+closeList.setAttribute("id", "closeList");
+closeList.textContent = "close";
+
+const titleCont = document.createElement("h3");
+titleCont.textContent = "Tus favoritos";
+
+likedMusicElement.appendChild(closeList);
+likedMusicElement.appendChild(titleCont);
+
+likedSongsModal.addEventListener("click", () => {
+  likedMusicElement.classList.add("showFavoriteSongs");
+  closeSideModal();
+});
+
+function showLikedMusic(likedMusic) {
+  likedMusicElement.innerHTML = "";
+
+  const closeList = document.createElement("span");
+  closeList.classList.add("material-icons", "closeList");
+  closeList.setAttribute("id", "closeList");
+  closeList.textContent = "close";
+
+  const titleCont = document.createElement("h3");
+  titleCont.textContent = "Tus favoritos";
+
+  likedMusicElement.appendChild(closeList);
+  likedMusicElement.appendChild(titleCont);
+
+  likedMusic.forEach((song) => {
+    const divCards = document.createElement("div");
+    divCards.classList.add("cards");
+
+    const divcardTxt = document.createElement("div");
+    divcardTxt.classList.add("card_text");
+    divcardTxt.setAttribute("id", "cardText");
+
+    const divImg = document.createElement("div");
+    divImg.classList.add("card_imagen");
+
+    const imgCard = document.createElement("img");
+    imgCard.src = song.img;
+
+    const cardTitle = document.createElement("h4");
+    cardTitle.classList.add("songName");
+    cardTitle.textContent = song.name;
+
+    const divSeparate = document.createElement("div");
+    divSeparate.classList.add("separate");
+
+    // const deleteSong = document.createElement("span");
+    // deleteSong.classList.add("material-icons", "deleteSong");
+    // deleteSong.setAttribute("id", "deleteSong");
+    // deleteSong.textContent = "delete";
+
+    divCards.appendChild(divImg);
+    divCards.appendChild(divcardTxt);
+    divCards.appendChild(imgCard);
+    divCards.appendChild(cardTitle);
+    divCards.appendChild(divSeparate);
+    // divCards.appendChild(deleteSong);
+
+    divImg.appendChild(imgCard);
+
+    divcardTxt.appendChild(cardTitle);
+
+    // divSeparate.appendChild(deleteSong);
+
+    likedMusicElement.appendChild(divCards);
+
+    closeSideModal();
+  });
+}
 
 // FUNCIONALIDAD DE BOTTON "PLAY/PLAUSE"
 playPause.addEventListener("click", () => {
@@ -229,73 +359,40 @@ audioSong.addEventListener("timeupdate", () => {
   timeStart.innerHTML = minutos(audioSong.currentTime);
 });
 
-progressBar.addEventListener("change", () => {
+progressBar.addEventListener("change", (e) => {
+  e.preventDefault();
   audioSong.currentTime = (progressBar.value * audioSong.duration) / 100;
 });
 
+// EVENTO PARA QUE SE REPITA AUTOMATICAMENTE LA CANCION
 audioSong.addEventListener("ended", () => {
-  if (songIndex >= 11) {
-    songIndex = 0;
-  } else {
-    songIndex += 1;
+  let switchIcon = loopSong.innerText;
+
+  switch (switchIcon) {
+    case "repeat_one":
+      songIndex -= 1;
+      break;
+
+    case "shuffle_on":
+      let randomIndex = Math.floor(Math.random() * songs.length + 1);
+      do {
+        randomIndex = Math.floor(Math.random() * songs.length + 1);
+      } while (songIndex === randomIndex);
+      songIndex = randomIndex;
+      break;
   }
-  audioSong.src = `audio/${songIndex + 1}.mp3`;
-  songName.innerText = `Now Playing: ${songs[songIndex].name}`;
-  timeDuration.innerText = songs[songIndex].duration;
-  audioSong.currentTime = 0;
-  audioSong.play();
 });
-
-// TIMER PARA LA BARRA DE PROGRESO
-
-// FLECHAS PREV/NEXT
-nextSong.addEventListener("click", () => {
-  if (songIndex >= 11) {
-    songIndex = 0;
-  } else {
-    songIndex += 1;
-  }
-  audioSong.src = `audio/${songIndex + 1}.mp3`;
-  songName.innerText = `Now Playing: ${songs[songIndex].name}`;
-  timeDuration.innerText = songs[songIndex].duration;
-  audioSong.currentTime = 0;
-  audioSong.play();
-  playPause.classList.remove("bi-play-circle-fill");
-  playPause.classList.add("bi-pause-circle-fill");
-});
-
-prevSong.addEventListener("click", () => {
-  if (songIndex <= 0) {
-    songIndex = 0;
-  } else {
-    songIndex -= 1;
-  }
-  audioSong.src = `audio/${songIndex + 1}.mp3`;
-  songName.innerText = `Now Playing: ${songs[songIndex].name}`;
-  timeDuration.innerText = songs[songIndex].duration;
-  audioSong.currentTime = 0;
-  audioSong.play();
-  playPause.classList.remove("bi-play-circle-fill");
-  playPause.classList.add("bi-pause-circle-fill");
-});
-// FLECHAS PREV/NEXT
-
-// REPLAY
-replay.addEventListener("click", () => {
-  audioSong.currentTime = 0;
-});
-// REPLAY
 
 loopSong.addEventListener("click", () => {
   let switchIcon = loopSong.innerText;
 
   switch (switchIcon) {
     case "repeat":
-      loopSong.innerHTML = "repeat_on";
+      loopSong.innerHTML = "repeat_one";
       loopSong.setAttribute("title", "Replay Automatico Activado");
       break;
 
-    case "repeat_on":
+    case "repeat_one":
       loopSong.innerHTML = "shuffle_on";
       loopSong.setAttribute("title", "Aleatorio");
       break;
@@ -307,115 +404,44 @@ loopSong.addEventListener("click", () => {
   }
 });
 
-// audioSong.addEventListener("ended", () => {
-//   let switchIcon = loopSong.innerText;
+audioSong.addEventListener("ended", () => {
+  if (songIndex >= 11) {
+    songIndex = 0;
+  } else {
+    songIndex += 1;
+  }
+  loadMusic();
+});
 
-//   switch (switchIcon) {
-//     case "repeat_on":
-//       audioSong.currentTime = 0;
-//       break;
+// TIMER PARA LA BARRA DE PROGRESO
 
-//     case "shuffle_on":
-//     // let randomIndex = Math.floor(Math.random() * songs.length + 1);
-//     // do {
-//     //   randomIndex = Math.floor(Math.random() * songs.length + 1);
-//     // } while ((songIndex = randomIndex));
-//     // songIndex = randomIndex;
-//     // audioSong.src = `audio/${songIndex + 1}.mp3`;
-//     // songName.innerText = `Now Playing: ${songs[songIndex].name}`;
-//     // timeDuration.innerText = songs[songIndex].duration;
-//     // audioSong.currentTime = 0;
-//     // audioSong.play();
-//     // break;
-//   }
-// });
+// FLECHAS PREV/NEXT
+nextSong.addEventListener("click", () => {
+  if (songIndex >= 11) {
+    songIndex = 0;
+  } else {
+    songIndex += 1;
+  }
+  loadMusic();
+});
 
-////////////////////////////////
+prevSong.addEventListener("click", () => {
+  if (songIndex <= 0) {
+    songIndex = 0;
+  } else {
+    songIndex -= 1;
+  }
+  loadMusic();
+});
+// FLECHAS PREV/NEXT
 
-const fadeContent = document.querySelector(".contenedor_degradado");
+// REPLAY
+replay.addEventListener("click", () => {
+  audioSong.currentTime = 0;
+});
+// REPLAY
 
-const mainDiv = document.createElement("div");
-mainDiv.classList.add("banner");
-
-const noBannerDiv = document.createElement("div");
-noBannerDiv.classList.add("contenedor_playlist");
-noBannerDiv.setAttribute("id", "slider");
-
-const sliderBtnRight = document.createElement("div");
-sliderBtnRight.classList.add("sliderBtn", "sliderBtnRight");
-sliderBtnRight.setAttribute("id", "btn_right");
-
-const sliderBtnLeft = document.createElement("div");
-sliderBtnLeft.classList.add("sliderBtn", "sliderBtnLeft");
-sliderBtnLeft.setAttribute("id", "btn_left");
-
-const sliderArrowR = document.createElement("i");
-sliderArrowR.classList.add("bi-arrow-right-circle-fill");
-
-const sliderArrowL = document.createElement("i");
-sliderArrowL.classList.add("bi-arrow-left-circle-fill");
-
-sliderBtnLeft.appendChild(sliderArrowL);
-sliderBtnRight.appendChild(sliderArrowR);
-
-mainDiv.appendChild(sliderBtnLeft);
-mainDiv.appendChild(sliderBtnRight);
-
-fadeContent.appendChild(mainDiv);
-fadeContent.appendChild(noBannerDiv);
-
-const bannerIndex = [
-  {
-    name: "defaultPlaylist",
-    src: "https://i.scdn.co/image/ab67706f00000002732148ed92f27fac784e3fd0",
-    id: "1",
-  },
-  {
-    name: "defaultPlaylist",
-    src: "https://i.scdn.co/image/ab67706f00000002799d54629fcb5e2031ceb774",
-    id: "2",
-  },
-  {
-    name: "defaultPlaylist",
-    src: "https://i.scdn.co/image/ab67706f000000020cf6ea047a23087c5a62a2ba",
-    id: "3",
-  },
-  {
-    name: "defaultPlaylist",
-    src: "https://i.scdn.co/image/ab67706f000000020e2ff2b38d2e24ec4a58654f",
-    id: "4",
-  },
-  {
-    name: "defaultPlaylist",
-    src: "https://i.scdn.co/image/ab67706f000000025373a723bc97f5f834648c2b",
-    id: "5",
-  },
-];
-
-function mainBanner() {
-  bannerIndex.forEach((banner) => {
-    const figuresContainer = document.createElement("div");
-    figuresContainer.classList.add("cancion");
-    const figure = document.createElement("figure");
-    const image = document.createElement("img");
-    image.src = banner.src;
-
-    mainDiv.appendChild(noBannerDiv);
-    mainDiv.appendChild(figuresContainer);
-    mainDiv.appendChild(figure);
-    mainDiv.appendChild(image);
-
-    noBannerDiv.appendChild(figuresContainer);
-    noBannerDiv.appendChild(figure);
-    noBannerDiv.appendChild(image);
-
-    figuresContainer.appendChild(figure);
-    figuresContainer.appendChild(image);
-
-    figure.appendChild(image);
-  });
-}
-
+// MODAL WINDOW FOR LOG IN
 const modalBtn = document.getElementById("modalButton");
 const modal = document.querySelector(".modalBody");
 const closeModal = document.getElementById("formIcon");
@@ -437,3 +463,4 @@ window.addEventListener("click", (element) => {
     modalShow.style.transform = "translateY(-200%)";
   }
 });
+// MODAL WINDOW FOR LOG IN
